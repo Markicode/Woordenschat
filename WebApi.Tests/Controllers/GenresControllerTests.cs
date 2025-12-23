d86eec5f77a2bb155ededa592b6aa768c20815f5
Markicode <markschwering@hotmail.com>
2025-12-23 13:58:32 +0100

    Finish GenresController tests.

--- WebApi.Tests/Controllers/GenresControllerTests.cs
+++ WebApi.Tests/Controllers/GenresControllerTests.cs
@@ -40,6 +40,64 @@ namespace WebApi.Tests.Controllers
 
             Assert.NotNull(genres);
             Assert.NotEmpty(genres);
+
+            Assert.All(genres!, genre =>
+            {
+                Assert.True(genre.Id > 0);
+                Assert.False(string.IsNullOrWhiteSpace(genre.Name));
+            });
+        }
+
+        [Fact]
+        public async Task GetGenreById_ReturnsOk()
+        {
+            // Act
+            var response = await _client.GetAsync("api/genres/1");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetGenreById_ReturnsGenre()
+        {
+            var response = await _client.GetAsync("api/genres/1");
+
+            response.EnsureSuccessStatusCode();
+
+            var genre = await response.Content.ReadFromJsonAsync<GenreDto>();
+
+            Assert.NotNull(genre);
+
+            Assert.True(genre.Id > 0);
+            Assert.False(string.IsNullOrWhiteSpace(genre.Name));
+        }
+
+        [Fact]
+        public async Task GetGenreById_ReturnsNotFound()
+        {
+            var response = await _client.GetAsync("api/genres/999");
+
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetGenres_AllParentGenresValid()
+        {
+            var response = await _client.GetAsync("api/genres");
+            response.EnsureSuccessStatusCode();
+            
+            var genres = await response.Content.ReadFromJsonAsync<List<GenreDto>>();
+            Assert.All(genres!, genre =>
+            {
+                if (genre.ParentGenreId.HasValue)
+                { 
+                    var parentGenre = genres!.FirstOrDefault(g => g.Id == genre.ParentGenreId.Value);
+                    Assert.NotNull(parentGenre);
+                    Assert.True(parentGenre.Id > 0);
+                    Assert.False(string.IsNullOrWhiteSpace(parentGenre.Name));
+                }
+            });
         }
     }
 }

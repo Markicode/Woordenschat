d202fbca2eeab3ae1e4e4220b7aafd71c20559e6
Mark Schwering <markschwering@hotmail.com>
2026-01-22 19:42:23 +0100

    style hero and navbar

--- frontend/web/src/components/hero/Hero.tsx
+++ frontend/web/src/components/hero/Hero.tsx
@@ -5,7 +5,10 @@ export default function Hero() {
     return (
     <section className={styles.hero}>
       <div className={styles.content}>
-        <h1><span className={styles.titleLeft}>Bibliotheek</span> <span className={styles.titleRight}>Woordenschat</span></h1>
+        <h1><img className={styles.logo} src="./src/assets/img/woordenschat-logo-png.png" alt="Woordenschat logo" />
+        <span className={styles.titleLeft}>Bibliotheek</span> 
+        
+        <span className={styles.titleRight}>Woordenschat</span></h1>
         <p>Ontdek de rijkdom van taal.</p>
       </div>
     </section>

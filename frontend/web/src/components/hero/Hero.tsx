25a9ba4f13a55573d619b83c660dc621f0e5ebcf
Mark Schwering <markschwering@hotmail.com>
2026-01-26 19:54:00 +0100

    Add pagebanner and adjust navbar

--- frontend/web/src/components/hero/Hero.tsx
+++ frontend/web/src/components/hero/Hero.tsx
@@ -1,14 +1,19 @@
-import styles from './Hero.module.css';
+import styles from "./Hero.module.css";
 
 export default function Hero() {
-
-    return (
-    <section className={styles.hero}>
+  return (
+    <section id="hero" className={styles.hero}>
       <div className={styles.content}>
-        <h1><img className={styles.logo} src="./src/assets/img/woordenschat-logo-png.png" alt="Woordenschat logo" />
-        <span className={styles.titleLeft}>Bibliotheek</span> 
-        
-        <span className={styles.titleRight}>Woordenschat</span></h1>
+        <h1>
+          <img
+            className={styles.logo}
+            src="./src/assets/img/woordenschat-logo-png.png"
+            alt="Woordenschat logo"
+          />
+          <span className={styles.titleLeft}>Bibliotheek</span>
+
+          <span className={styles.titleRight}>Woordenschat</span>
+        </h1>
         <p>Ontdek de rijkdom van taal.</p>
       </div>
     </section>
